02324f11fdf3b9d94207a86ad3b9179a84cf9ab4
Efe Ali GULOCAK <aliefegulocak@gmail.com>
2019-05-20 15:55:58 +0300

    add challenge page

---
+++ pages/challenge.vue
@@ -0,0 +1,44 @@
+<template>
+<b-container fluid>
+  <p id="Bravado">BRAVADO</p>
+  <b-row class="justify-content-md-center">
+    <b-button size="lg" variant="primary">Submit Assignment</b-button>
+    
+
+      </b-row>
+      <hr>
+      <b-row class="justify-content-md-center"> 
+          <b-col md="6">
+                <b-card no-body>
+    <b-tabs pills card>
+      <b-tab title="About" active><b-card-text>Tab Contents 1<br></b-card-text></b-tab>
+      <b-tab title="Feeds"><b-card-text>Tab Contents 2</b-card-text></b-tab>
+      <b-tab title="Participiants"><b-card-text>Tab Contents 3</b-card-text></b-tab>
+    </b-tabs>
+  </b-card>
+
+              </b-col>
+          </b-row>
+          <hr>
+          <b-row class="justify-content-md-center">
+              <b-button size="lg" variant="danger">Give up challenge</b-button>
+              </b-row>
+</b-container>
+
+
+
+
+
+
+</template>
+<style>
+#Bravado{
+  font-size: 50px;
+  text-align: center;
+  background-color: lightblue;
+  color: black;
+  padding: 40px;
+}
+
+
+</style>
